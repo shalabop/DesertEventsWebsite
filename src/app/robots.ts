c6ad0bc521c5abbc63ebadabdb66331ed5c8c282
afazzam <afazzam@asu.edu>
2025-12-22 21:51:28 -0700

    Add default exports for robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,12 @@
-﻿
+﻿import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://deserteventsaz.com/sitemap.xml',
+  }
+}
